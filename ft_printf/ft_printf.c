446a955e0d6e8f866045c1c4d28b7e1817238f6c
Raphael Rouiller <rrouille@c1r7s8.42lausanne.ch>
2023-08-12 16:32:44 +0200

    Moulinette OK

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 11:11:32 by rrouille          #+#    #+#             */
-/*   Updated: 2023/08/12 15:47:10 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/08/12 16:28:33 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int	print_nbr_base(int nbr, int base, const char *str_base)
 {
@@ -36,13 +36,13 @@ int	print_nbr_base(int nbr, int base, const char *str_base)
 
 int	print_str(char *str)
 {
-	int		len;
+	int	len;
 
 	len = 0;
 	if (!str)
 	{
 		write(1, "(null)", 6);
-		return (6);
+		return (len + 6);
 	}
 	while (*str)
 	{
@@ -75,7 +75,7 @@ int	print_nbr(int nbr)
 int	print_hex(unsigned int hex)
 {
 	int	len;
-	
+
 	len = 0;
 	if (hex == 2147483648)
 	{
@@ -101,31 +101,33 @@ int	ft_printf(const char *arg, ...)
 	{
 		if (*arg == '%')
 		{
-			switch (*++arg)
-			{
-			case 's':
-				len += print_str(va_arg(ap, char*));
-				break;
-			case 'd':
+			if (*++arg == 'd')
 				len += print_nbr(va_arg(ap, int));
-				break;
-			case 'x':
+			else if (*arg == 'x')
 				len += print_hex(va_arg(ap, unsigned int));
-				break;
-			default:
+			else if (*arg == 's')
+				len += print_str(va_arg(ap, char *));
+			else
+			{
 				write(1, "%", 1);
 				write(1, arg, 1);
 				len++;
-				break;
 			}
 		}
 		else
 		{
-			len++;
 			write(1, arg, 1);
+			len++;
 		}
 		arg++;
 	}
 	va_end(ap);
 	return (len);
 }
+
+// int	main(void)
+// {
+// 	ft_printf("%s\n", "Hello World!");
+// 	ft_printf("%d\n", 42);
+// 	ft_printf("%x\n", 42);
+// }
--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,52 +1,60 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 16:20:57 by rrouille          #+#    #+#             */
-/*   Updated: 2023/06/08 16:24:05 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/08/12 16:18:20 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
+	int		i;
+	int		rd;
+	char	character;
+	char	*buffer;
+
+	i = 0;
+	rd = 0;
 	if (BUFFER_SIZE <= 0)
 		return (NULL);
-	int     i = 0;
-	int     rd = 0;
-	char    character;
-	char    *buffer = malloc(100000);
-
-	while ((rd = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1)) > 0)
+	buffer = malloc(100000);
+	rd = read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1);
+	while (rd > 0)
 	{
 		buffer[i++] = character;
 		if (character == '\n')
 			break ;
 	}
-	buffer[i] =  '\0';
+	buffer[i] = '\0';
 	if (rd == -1 || i == 0 || (!buffer[i - 1] && !rd))
 		return (free(buffer), NULL);
-	return(buffer);
+	return (buffer);
 }
 
-int main() {
-	int fd = open("example.txt", O_RDONLY);
-	if (fd == -1) {
-		perror("Failed to open file");
-		return 1;
-	}
+int	main(void)
+{
+	char	*line;
+	int		fd;
 
-	char *line = NULL;
-	while ((line = get_next_line(fd)) != NULL) {
+	fd = open("example.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Failed to open file");
+		return (1);
+	}
+	line = NULL;
+	line = get_next_line(fd);
+	while (!line)
+	{
 		printf("%s\n", line);
 		free(line);
 	}
-
 	close(fd);
-
-	return 0;
+	return (0);
 }
